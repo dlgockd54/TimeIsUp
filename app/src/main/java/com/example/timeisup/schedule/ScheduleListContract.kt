81e8bbc2f6406c4aa71934563030c6c5a8f528a4
hclee <dlgockd45@gmail.com>
2019-05-15 18:54:05 +0900

    Create ScheduleListContract, ScheduleListPresenter

---
+++ app/src/main/java/com/example/timeisup/schedule/ScheduleListContract.kt
@@ -0,0 +1,18 @@
+package com.example.timeisup.schedule
+
+import com.example.hclee.lifeguard.BasePresenter
+import com.example.hclee.lifeguard.BaseView
+
+/**
+ * Created by hclee on 2019-05-15.
+ */
+
+interface ScheduleListContract {
+    interface View: BaseView<Presenter> {
+
+    }
+
+    interface Presenter: BasePresenter {
+
+    }
+}
---
+++ app/src/main/java/com/example/timeisup/schedule/ScheduleListPresenter.kt
@@ -0,0 +1,9 @@
+package com.example.timeisup.schedule
+
+/**
+ * Created by hclee on 2019-05-15.
+ */
+
+class ScheduleListPresenter: ScheduleListContract.Presenter {
+
+}
