85de5dc686520e6d3a80149588aa1f1195c432ee
hclee <dlgockd45@gmail.com>
2019-05-15 18:55:21 +0900

    Modify ScheduleListActivity to extend base class

--- app/src/main/java/com/example/timeisup/schedule/ScheduleListActivity.kt
+++ app/src/main/java/com/example/timeisup/schedule/ScheduleListActivity.kt
@@ -2,13 +2,21 @@ package com.example.timeisup.schedule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.hclee.lifeguard.BaseView
 import com.example.timeisup.R
 
-class ScheduleListActivity : AppCompatActivity() {
+class ScheduleListActivity : AppCompatActivity(), ScheduleListContract.View {
     private val TAG: String = ScheduleListActivity::class.java.simpleName
+
+    override lateinit var mPresenter: ScheduleListContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule_list)
+
+        Log.d(TAG, "onCreate()")
+
+
     }
 }
