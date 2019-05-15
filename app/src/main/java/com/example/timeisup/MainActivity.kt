ee651e907609ff14dafdac19524eda32ddbd05b3
hclee <dlgockd45@gmail.com>
2019-05-15 19:17:38 +0900

    Start ScheduleListActivity then finish()

--- app/src/main/java/com/example/timeisup/MainActivity.kt
+++ app/src/main/java/com/example/timeisup/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.timeisup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.timeisup.schedule.ScheduleListActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        startActivity(Intent(this, ScheduleListActivity::class.java))
+        finish()
     }
 }
