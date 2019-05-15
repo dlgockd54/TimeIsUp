835a10aab9378da9277a47c80cabee3ecdd8b0b7
hclee <dlgockd45@gmail.com>
2019-05-15 19:06:05 +0900

    Add RecyclerView in ScheduleListActivity

--- app/src/main/java/com/example/timeisup/schedule/ScheduleListActivity.kt
+++ app/src/main/java/com/example/timeisup/schedule/ScheduleListActivity.kt
@@ -2,14 +2,18 @@ package com.example.timeisup.schedule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import com.example.hclee.lifeguard.BaseView
 import com.example.timeisup.R
+import kotlinx.android.synthetic.main.activity_schedule_list.*
 
 class ScheduleListActivity : AppCompatActivity(), ScheduleListContract.View {
     private val TAG: String = ScheduleListActivity::class.java.simpleName
 
     override lateinit var mPresenter: ScheduleListContract.Presenter
+
+    lateinit var mRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,6 @@ class ScheduleListActivity : AppCompatActivity(), ScheduleListContract.View {
 
         Log.d(TAG, "onCreate()")
 
-
+        mRecyclerView = rv_schedule_list
     }
 }
