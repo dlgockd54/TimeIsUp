0d372ced29c7d1f9863e2c8b49a928ea5e68205a
hclee <dlgockd45@gmail.com>
2019-05-17 22:36:26 +0900

    Create ScheduleAddingActivity

---
+++ app/src/main/java/com/example/timeisup/scheduleadding/ScheduleAddingActivity.kt
@@ -0,0 +1,27 @@
+package com.example.timeisup.scheduleadding
+
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.util.Log
+import com.example.timeisup.BaseActivity
+import com.example.timeisup.R
+import kotlinx.android.synthetic.main.activity_schedule_adding.*
+
+class ScheduleAddingActivity : BaseActivity() {
+    private val TAG: String = ScheduleAddingActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_adding)
+        setSupportActionBar(toolbar_schedule_adding as Toolbar)
+
+        Log.d(TAG, "onCreate()")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        Log.d(TAG, "onBackPressed()")
+        overridePendingTransition(R.anim.animation_slide_from_left, R.anim.animation_slide_to_right)
+    }
+}
