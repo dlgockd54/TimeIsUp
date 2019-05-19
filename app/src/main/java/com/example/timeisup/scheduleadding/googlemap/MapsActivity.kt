385774abe8ef95aa30d8f51d6ad910b5ba59963c
hclee <dlgockd45@gmail.com>
2019-05-19 18:22:26 +0900

    Add marker on current coordinates

--- app/src/main/java/com/example/timeisup/scheduleadding/googlemap/MapsActivity.kt
+++ app/src/main/java/com/example/timeisup/scheduleadding/googlemap/MapsActivity.kt
@@ -1,21 +1,42 @@
 package com.example.timeisup.scheduleadding.googlemap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.timeisup.R
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val TAG: String = MapsActivity::class.java.simpleName
+    private val UPDATE_INTERVAL: Long = 1000 * 30
+    private val FASTEST_UPDATE_INTERVAL: Long = 1000 * 10
 
     private lateinit var mMap: GoogleMap
+    private var mCurrentMarker: Marker? = null
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mCurrentLocation: Location
+    private lateinit var mCurrentCoordinates: LatLng
+    private val mLocationCallback: LocationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            Log.d(TAG, "onLocationResult()")
+
+            locationResult?.lastLocation?.let {
+                Log.d(TAG, "last location: ${it.latitude}, longitude: ${it.longitude}")
+
+                setCurrentLocation(it)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +48,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        val locationSettingsRequestBuilder: LocationSettingsRequest.Builder
+                = LocationSettingsRequest.Builder()
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        mLocationRequest = LocationRequest().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = UPDATE_INTERVAL
+            fastestInterval = FASTEST_UPDATE_INTERVAL
+        }
+
+        locationSettingsRequestBuilder.addLocationRequest(mLocationRequest)
     }
 
     /**
@@ -47,6 +80,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null)
+            }
+        }
+    }
+
+    private fun setCurrentLocation(location: Location) {
+        mCurrentLocation = location
+        mCurrentCoordinates = LatLng(mCurrentLocation.latitude, mCurrentLocation.longitude)
+
+        setMarker()
+    }
+
+    private fun setMarker() {
+        val cameraUpdate: CameraUpdate = CameraUpdateFactory.newLatLng(mCurrentCoordinates)
+        val markerOptions: MarkerOptions = MarkerOptions().apply {
+            position(mCurrentCoordinates)
+            title("현재 위치")
+            draggable(true)
+        }
+
+        mCurrentMarker = mCurrentMarker ?: mMap.addMarker(markerOptions)
+        mMap.moveCamera(cameraUpdate)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+
+        finish()
+        overridePendingTransition(R.anim.animation_slide_from_left, R.anim.animation_slide_to_right)
     }
 
     override fun onResume() {
