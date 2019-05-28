f2f34b01d547ae8dc9335e33a8c7f107b5565b3c
hclee <dlgockd45@gmail.com>
2019-05-28 20:12:43 +0900

    Add listeners for removing schedule

--- app/src/main/java/com/example/timeisup/firebase/FirebaseManager.kt
+++ app/src/main/java/com/example/timeisup/firebase/FirebaseManager.kt
@@ -3,6 +3,10 @@ package com.example.timeisup.firebase
 import android.util.Log
 import com.example.timeisup.schedule.Schedule
 import com.example.timeisup.schedule.ScheduleListPresenter
+import com.google.android.gms.tasks.OnCanceledListener
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.database.*
 
 /**
@@ -14,21 +18,25 @@ object FirebaseManager {
 
     private val mFirebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
     private val mDatabaseReference: DatabaseReference = mFirebaseDatabase.reference
+    private val onSuccessListener: OnSuccessListener<Void> = OnSuccessListener {
+        Log.d(TAG, "onSuccess()")
+    }
+    private val onFailureListener: OnFailureListener = OnFailureListener {
+        Log.d(TAG, "onFailure()")
+    }
+    private val onCompleteListener: OnCompleteListener<Void> = OnCompleteListener {
+        Log.d(TAG, "onComplete()")
+    }
+    private val onCanceledListener: OnCanceledListener = OnCanceledListener {
+        Log.d(TAG, "onCanceled()")
+    }
 
     fun addScheduleToDatabase(schedule: Schedule) {
         mDatabaseReference.child(ScheduleListPresenter.DB_NODE_NAME).push().setValue(schedule).let {
-            it.addOnSuccessListener {
-                Log.d(TAG, "onSuccess()")
-            }
-            it.addOnFailureListener {
-                Log.d(TAG, "onFailure()")
-            }
-            it.addOnCompleteListener {
-                Log.d(TAG, "onComplete()")
-            }
-            it.addOnCanceledListener {
-                Log.d(TAG, "onCanceled()")
-            }
+            it.addOnSuccessListener(onSuccessListener)
+            it.addOnFailureListener(onFailureListener)
+            it.addOnCompleteListener(onCompleteListener)
+            it.addOnCanceledListener(onCanceledListener)
         }
     }
 
@@ -36,7 +44,12 @@ object FirebaseManager {
         Log.d(TAG, "removeScheduleFromDatabase()")
         Log.d(TAG, "key: $key")
 
-        mDatabaseReference.child(ScheduleListPresenter.DB_NODE_NAME).child(key).removeValue()
+        mDatabaseReference.child(ScheduleListPresenter.DB_NODE_NAME).child(key).removeValue().let {
+            it.addOnSuccessListener(onSuccessListener)
+            it.addOnFailureListener(onFailureListener)
+            it.addOnCompleteListener(onCompleteListener)
+            it.addOnCanceledListener(onCanceledListener)
+        }
     }
 
     fun addChildEventListener(listener: ChildEventListener) {
